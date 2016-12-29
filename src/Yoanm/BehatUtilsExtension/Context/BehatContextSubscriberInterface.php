7abe4259052e17262040f049422f01047e0ae849
Yoanm <yoanm@users.noreply.github.com>
2016-12-29 17:19:15 +0100

    Fix

--- src/Yoanm/BehatUtilsExtension/Context/BehatContextSubscriberInterface.php
+++ src/Yoanm/BehatUtilsExtension/Context/BehatContextSubscriberInterface.php
@@ -5,8 +5,8 @@ use Behat\Behat\Context\Context;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Want to listen Behat events
- * Just implement this interface and your context will be passed to Behat dispatcher
+ * Want to listen Behat events (including those extension ones)
+ * Just implement this interface your context will be passed to Behat dispatcher
  */
 interface BehatContextSubscriberInterface extends Context, EventSubscriberInterface
 {
